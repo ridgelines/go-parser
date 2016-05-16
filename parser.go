7b63a4cc96cc2f9a242869d3a2f3ad610fcf7a23
Jett (SEA) <jett.jones@us.imshealth.com>
2016-05-16 15:03:53 -0700

    Add support for Interfaces and Imports

--- parser.go
+++ parser.go
@@ -1,6 +1,7 @@
 package parser
 
 import (
+	"fmt"
 	"go/ast"
 	"go/parser"
 	"go/token"
@@ -24,7 +25,7 @@ func ParseFile(path string) (*GoFile, error) {
 		Structs: []*GoStruct{},
 	}
 
-	// File.Decls: A list of the delcarations in the file: https://golang.org/pkg/go/ast/#Decl
+	// File.Decls: A list of the declarations in the file: https://golang.org/pkg/go/ast/#Decl
 	for _, decl := range file.Decls {
 		switch declType := decl.(type) {
 
@@ -47,9 +48,18 @@ func ParseFile(path string) (*GoFile, error) {
 					case (*ast.StructType):
 						structType := typeSpecType
 						goFile.Structs = append(goFile.Structs, buildGoStruct(source, goFile, typeSpec, structType))
+					// InterfaceType: An InterfaceType node represents an interface type. https://golang.org/pkg/go/ast/#InterfaceType
+					case (*ast.InterfaceType):
+						interfaceType := typeSpecType
+						goFile.Interfaces = append(goFile.Interfaces, buildGoInterface(source, goFile, typeSpec, interfaceType))
 					default:
 						// a not-implemented typeSpec.Type.(type), ignore
 					}
+					// ImportSpec: An ImportSpec node represents a single package import. https://golang.org/pkg/go/ast/#ImportSpec
+				case *ast.ImportSpec:
+					importSpec := genSpec.(*ast.ImportSpec)
+					goImport := buildGoImport(importSpec, goFile)
+					goFile.Imports = append(goFile.Imports, goImport)
 				default:
 					// a not-implemented genSpec.(type), ignore
 				}
@@ -60,6 +70,131 @@ func ParseFile(path string) (*GoFile, error) {
 	}
 
 	return goFile, nil
+}
+
+func buildGoImport(spec *ast.ImportSpec, file *GoFile) *GoImport {
+	name := ""
+	if spec.Name != nil {
+		name = spec.Name.Name
+	}
+	path := ""
+	if spec.Path != nil {
+		path = spec.Path.Value
+	}
+
+	return &GoImport{
+		Name: name,
+		Path: path,
+		File: file,
+	}
+}
+
+func buildGoInterface(source []byte, file *GoFile, typeSpec *ast.TypeSpec, interfaceType *ast.InterfaceType) *GoInterface {
+	goInterface := &GoInterface{
+		File:    file,
+		Name:    typeSpec.Name.Name,
+		Methods: buildMethodList(interfaceType.Methods.List, source),
+	}
+	return goInterface
+}
+
+func buildMethodList(fieldList []*ast.Field, source []byte) []*GoMethod {
+	methods := []*GoMethod{}
+
+	for _, field := range fieldList {
+		name := getNames(field)[0]
+
+		fType, ok := field.Type.(*ast.FuncType)
+		if !ok {
+			// method was not a function
+			continue
+		}
+
+		goMethod := &GoMethod{
+			Name:    name,
+			Params:  buildTypeList(fType.Params, source),
+			Results: buildTypeList(fType.Results, source),
+		}
+		methods = append(methods, goMethod)
+	}
+	return methods
+}
+
+func buildTypeList(fieldList *ast.FieldList, source []byte) []*GoType {
+	types := []*GoType{}
+
+	if fieldList != nil {
+		for _, t := range fieldList.List {
+			goType := buildType(t.Type, source)
+			for _, n := range getNames(t) {
+				copyType := copyType(goType)
+				copyType.Name = n
+				types = append(types, copyType)
+			}
+		}
+	}
+	return types
+}
+
+func getNames(field *ast.Field) []string {
+	if field.Names == nil || len(field.Names) == 0 {
+		return []string{""}
+	}
+	result := []string{}
+	for _, name := range field.Names {
+		result = append(result, name.String())
+	}
+	return result
+}
+
+func getTypeString(expr ast.Expr, source []byte) string {
+	return string(source[expr.Pos()-1 : expr.End()-1])
+}
+
+func copyType(goType *GoType) *GoType {
+	return &GoType{
+		Type:  goType.Type,
+		Inner: goType.Inner,
+		Name:  goType.Name,
+	}
+}
+
+func buildType(expr ast.Expr, source []byte) *GoType {
+	innerTypes := []*GoType{}
+	typeString := getTypeString(expr, source)
+
+	switch specType := expr.(type) {
+	case *ast.FuncType:
+		innerTypes = append(innerTypes, buildTypeList(specType.Params, source)...)
+		innerTypes = append(innerTypes, buildTypeList(specType.Results, source)...)
+	case *ast.ArrayType:
+		innerTypes = append(innerTypes, buildType(specType.Elt, source))
+	case *ast.MapType:
+		innerTypes = append(innerTypes, buildType(specType.Key, source))
+		innerTypes = append(innerTypes, buildType(specType.Value, source))
+	case *ast.ChanType:
+		innerTypes = append(innerTypes, buildType(specType.Value, source))
+	case *ast.StarExpr:
+		innerTypes = append(innerTypes, buildType(specType.X, source))
+	case *ast.Ellipsis:
+		innerTypes = append(innerTypes, buildType(specType.Elt, source))
+	case *ast.InterfaceType:
+		methods := buildMethodList(specType.Methods.List, source)
+		for _, m := range methods {
+			innerTypes = append(innerTypes, m.Params...)
+			innerTypes = append(innerTypes, m.Results...)
+		}
+
+	case *ast.Ident:
+	case *ast.SelectorExpr:
+	default:
+		fmt.Printf("Unexpected field type: `%s`,\n %#v\n", typeString, specType)
+	}
+
+	return &GoType{
+		Type:  typeString,
+		Inner: innerTypes,
+	}
 }
 
 func buildGoStruct(source []byte, file *GoFile, typeSpec *ast.TypeSpec, structType *ast.StructType) *GoStruct {
